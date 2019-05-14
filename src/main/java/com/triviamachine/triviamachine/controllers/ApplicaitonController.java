7bf5f7829bf68f6566ce955d7cb05d684034cef0
Dana Voros <vorosdana@gmail.com>
2019-05-14 09:08:25 -0700

    Index route added to controller

--- src/main/java/com/triviamachine/triviamachine/controllers/ApplicaitonController.java
+++ src/main/java/com/triviamachine/triviamachine/controllers/ApplicaitonController.java
@@ -1,14 +1,12 @@
 package com.triviamachine.triviamachine.controllers;
 
 import com.triviamachine.triviamachine.database.*;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping
@@ -29,4 +27,9 @@ public class ApplicaitonController {
         return "login";
     }
 
+    @GetMapping("")
+    public String getFrontPage(@AuthenticationPrincipal AdminUser user, Model model){
+        model.addAttribute("user", user);
+        return "index";
+    }
 }
