31ce292ff8de1d60ca312c54eecade1441a47767
Dana Voros <vorosdana@gmail.com>
2019-05-15 12:38:38 -0700

    Added QuestionSchedule.isAfterToday(), used functionality in admin/schedule.html to avoid constraint errors from invalid deletes

--- src/main/java/com/triviamachine/triviamachine/database/QuestionSchedule.java
+++ src/main/java/com/triviamachine/triviamachine/database/QuestionSchedule.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.*;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Entity
 public class QuestionSchedule {
@@ -36,6 +38,13 @@ public class QuestionSchedule {
 
     public String getDateToString() {
         return new SimpleDateFormat("yyyy-MM-dd").format(date);
+    }
+
+    public boolean isAfterToday() throws ParseException {
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date today = formatter.parse(formatter.format(new Date()));
+        today = new Date(today.getTime() + TimeUnit.DAYS.toMillis(1));
+        return this.date.after(today);
     }
 
     public void setDate(Date date) {
